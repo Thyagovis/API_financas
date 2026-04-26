56ef601b5df1350d6ae6e3081e6f80461d19883a
thyagovis <thyagosilva321123@gmail.com>
2026-04-25 21:17:12 -0300

    update

--- database.py
+++ database.py
@@ -2,7 +2,12 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import event
 
-DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
     DATABASE_URL,
